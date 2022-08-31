0f93e747fed60b2dd7d5565c5933765ab2d6b2fa
StevenAvilaS7 <steven.avila.rojas@gmail.com>
2022-08-31 13:02:33 -0500

    The project is loaded
    Se carga el proyecto

---
+++ Magnetism/Assets/Scrips/ChargedParticle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargedParticle : MonoBehaviour
+{
+    public float charge=1;
+
+    void Start(){
+        UpdateColor();
+    }
+
+    public void UpdateColor(){
+        Color color = charge > 0 ? Color.green : Color.red;
+        GetComponent<Renderer>().material.color = color;
+    }
+}
---
+++ Magnetism/Assets/Scrips/ParticleManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    private float cycleInterval=0.01f;
+    private List<ChargedParticle> chargedParticles;
+    private List<MovingChargedParticle> movingChargedParticles;
+
+    void Start()
+    {
+        chargedParticles = new List<ChargedParticle> (FindObjectsOfType<ChargedParticle>());
+        movingChargedParticles= new List<MovingChargedParticle> (FindObjectsOfType<MovingChargedParticle>());
+        
+        foreach (MovingChargedParticle mcp in movingChargedParticles){
+            StartCoroutine (Cycle (mcp));
+        }
+    }
+
+    public IEnumerator Cycle(MovingChargedParticle mcp){
+        bool isFirts= true;
+        while(true){
+            if(isFirts){
+                isFirts=false;
+                yield return new WaitForSeconds(Random.Range (0,cycleInterval));
+            }
+            ApplyMagneticForce(mcp);
+            yield return new WaitForSeconds(cycleInterval);
+        }
+    }
+
+    private void ApplyMagneticForce(MovingChargedParticle mcp){
+        Vector3 newForce = Vector3.zero;
+
+        foreach(ChargedParticle cp in chargedParticles){
+            if (mcp==cp)
+                continue;
+
+            float distance= Vector3.Distance(mcp.transform.position,cp.gameObject.transform.position);
+            float force=1000*mcp.charge*cp.charge/Mathf.Pow (distance,2);
+            Vector3 direction = mcp.transform.position-cp.transform.position;
+            direction.Normalize();
+
+            newForce+=force*direction*cycleInterval;
+
+            if (float.IsNaN(newForce.x))
+                newForce=Vector3.zero;
+
+            mcp.rb.AddForce(newForce);
+        }
+    }
+
+
+}
