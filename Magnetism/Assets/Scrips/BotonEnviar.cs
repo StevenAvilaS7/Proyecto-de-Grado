5a52f5dbb54c37b0ca4b8b4cd30277a21e9f8425
StevenAvilaS7 <steven.avila.rojas@gmail.com>
2022-10-26 22:40:28 -0500

    Se Configura boton
    se hace la configuración del boton que acciona la escena

---
+++ Magnetism/Assets/Scrips/BotonEnviar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonEnviar : MonoBehaviour
+{
+    public GameObject plane;
+
+    public void enviar()
+    {   
+        Debug.Log("Me pulsaste");
+        plane.gameObject.SetActive(true);
+    }
+
+}
