6f8e9b499d16b3c5aefa9d8d3b3be9f2510bbf63
StevenAvilaS7 <steven.avila.rojas@gmail.com>
2022-11-03 00:39:23 -0500

    Interfaz de datos
    Toma de datos con Sliders

--- Magnetism/Assets/Scrips/ChargedParticle.cs
+++ Magnetism/Assets/Scrips/ChargedParticle.cs
@@ -34,7 +34,56 @@ public class ChargedParticle : MonoBehaviour
         Start();
         Debug.Log("Se entra barra 2");
     }
+    public void SetMasaEsfera(float value)
+	{
+		OnValueChanged(value, 0);
+	}
+	
+	public void SetCargaEsfera(float value)
+	{
+		OnValueChanged(value, 1);
+	}
+	
+	public void SetCargaBarraSuperior(float value)
+	{
+		OnValueChanged(value, 2);
+	}
 
+    public void SetCargaBarraInferior(float value)
+	{
+		OnValueChanged(value, 3);
+	}
+
+    public void OnValueChanged(float value, int channel)
+	{
+		Color c = Color.white;
+
+		if (channel==0){
+        charge = value;
+        Start();
+        Debug.Log("Se entra a masa");
+        }
+        if(channel==1){
+        charge = value;
+        Start();
+        Debug.Log("Se entra carga esfera");
+        }
+        if (channel==2){
+        charge = value;
+        Start();
+        Debug.Log("Se entra a Barra Superior");
+        }
+        if(channel==3){
+        charge = value;
+        Start();
+        Debug.Log("Se entra a barra Inferior");
+        }
+	}
 }
+
+	
+	
+
+
 
 
